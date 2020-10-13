99e38a4bbeed6f3aee7c2d7dcd3037a84d61a5b1
DVigas <dviga@outlook.com>
2020-10-13 19:05:41 +0100

    Adicionando pagina home no React native

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -9,7 +9,7 @@ import {
 } from '@expo-google-fonts/play';
 
 import Header from './src/components/Header';
-import Home from './src/pages/Home';
+import Routes from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -23,7 +23,7 @@ export default function App() {
     return (
       <View style={styles.container}>
         <Header />
-        <Home />
+        <Routes />
         <StatusBar style="auto" />
       </View>
     );
---
+++ front-mobile/src/routes.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+import Home from './pages/Home';
+
+const Routes = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
+        </NavigationContainer>
+
+    )
+};
+
+export default Routes;
